c3a90cad695c76e72d8e0351713bcd22a3b8dc37
MaulingMonkey <git@maulingmonkey.com>
2019-12-29 17:42:57 -0800

    Improve MSRV (1.39.0) testing

--- scripts/install.cmd
+++ scripts/install.cmd
@@ -2,6 +2,7 @@
 curl -sSf -o "%TEMP%\rustup-init.exe" https://win.rustup.rs
 "%TEMP%\rustup-init.exe" --default-toolchain stable -y
 @set "PATH=%USERPROFILE%\.cargo\bin;%PATH%"
+rustup toolchain install 1.39.0     || exit /b 1
 rustup toolchain install stable     || exit /b 1
 rustup toolchain install beta       || exit /b 1
 rustup toolchain install nightly    || exit /b 1
--- scripts/test.cmd
+++ scripts/test.cmd
@@ -2,6 +2,7 @@
 @if defined CI set "PATH=%USERPROFILE%\.cargo\bin;%PATH%"
 @echo on
 @pushd "%~dp0.."
+cargo +1.39.0   build --all || @exit /b 1
 cargo +stable   build --all || @exit /b 1
 cargo +beta     build --all || @exit /b 1
 cargo +nightly  build --all || @exit /b 1
