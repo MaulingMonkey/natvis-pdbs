66151eeab5258493baea99ace94166a9df8beadb
MaulingMonkey <git@maulingmonkey.com>
2022-09-06 16:55:14 -0700

    scripts\install.cmd: skip rustup if already installed

--- scripts/install.cmd
+++ scripts/install.cmd
@@ -1,7 +1,9 @@
 @setlocal
+@rustup --version >NUL 2>NUL && goto :skip_rustup
 curl -sSf -o "%TEMP%\rustup-init.exe" https://win.rustup.rs
 "%TEMP%\rustup-init.exe" --default-toolchain stable -y
 @set "PATH=%USERPROFILE%\.cargo\bin;%PATH%"
+:skip_rustup
 rustup toolchain install 1.39.0     || exit /b 1
 rustup toolchain install stable     || exit /b 1
 rustup toolchain install beta       || exit /b 1
